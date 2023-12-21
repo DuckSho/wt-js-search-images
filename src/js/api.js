1ad0f5325865d97e031ddfe370fb3ea7f5c85839
DuckSho <profet12762@gmail.com>
2023-12-21 16:59:03 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '41256071-ed92c4fb3a3837c10316edc61';
+const API_KEY = '41387442-7044f1833e67eea60c3e4e56b';
 const BASE_URL = 'https://pixabay.com/api/';
 const DEFAULT_PER_PAGE = 40;
 
